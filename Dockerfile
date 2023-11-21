7f452e30dc8e1c06b868baf6f322dfb7cbd49199
Chanh Ly <chanhly.vn@gmail.com>
2023-11-21 10:38:54 +0700

    feat: add panic route

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN cd /src && go build -o goapp
 FROM alpine
 WORKDIR /app
 COPY --from=build-env /src/goapp /app/
+EXPOSE 80
 ENTRYPOINT ./goapp
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 
@@ -13,7 +14,7 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, "Hello, Docker! <3")
@@ -22,6 +23,16 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
+
+	e.GET("/panic", func(c echo.Context) error {
+		err := c.Echo().Shutdown(context.Background())
+    if err != nil {
+        if err != http.ErrServerClosed {
+            c.Echo().Logger.Fatal("shutting down the server")
+        }
+}
+    return nil
+	});
 
 	httpPort := os.Getenv("PORT")
 	if httpPort == "" {
